93b99a51a40c81658c1c3b8c9ea9a1cf0f1410af
Marcel Grünauer <marcel@cpan.org>
2010-03-27 10:16:08 +0000

    Import of MARCEL/File-Find-Upwards-1.100860 from CPAN.
    gitpan-cpan-distribution: File-Find-Upwards
    gitpan-cpan-version:      1.100860
    gitpan-cpan-path:         MARCEL/File-Find-Upwards-1.100860.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/File/Find/Upwards.pm
+++ lib/File/Find/Upwards.pm
@@ -1,11 +1,13 @@
-package File::Find::Upwards;
 use 5.008;
 use strict;
 use warnings;
+
+package File::Find::Upwards;
+our $VERSION = '1.100860';
+# ABSTRACT: Look for a file in the current directory and upwards
 use Path::Class;
 use Attribute::Memoize;
-our $VERSION = '0.04';
-use base 'Exporter';
+use Exporter qw(import);
 our @EXPORT = qw(file_find_upwards find_containing_dir_upwards);
 
 sub file_find_upwards : Memoize {
@@ -43,11 +45,18 @@ sub find_containing_dir_upwards : Memoize {
     undef;
 }
 1;
+
+
 __END__
+=pod
 
 =head1 NAME
 
 File::Find::Upwards - Look for a file in the current directory and upwards
+
+=head1 VERSION
+
+version 1.100860
 
 =head1 SYNOPSIS
 
@@ -60,9 +69,9 @@ File::Find::Upwards - Look for a file in the current directory and upwards
 
 Provides functions that can find a file in the current or a parent directory.
 
-=over 4
+=head1 FUNCTIONS
 
-=item C<file_find_upwards()>
+=head2 file_find_upwards
 
 Takes a filename and looks for the file in the current directory. If there is
 no such file, it traverses up the directory hierarchy until it finds the file
@@ -74,41 +83,46 @@ filename will return the result of the first call for that filename.
 
 This function is exported automatically.
 
-=item C<find_containing_dir_upwards()>
+=head2 find_containing_dir_upwards
 
 Like C<file_find_upwards()>, but reports the directory that contains the file.
 A C<Path::Class::Dir> object is returned.
 
 This function is exported automatically.
 
-=back
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=File-Find-Upwards>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://search.cpan.org/dist/File-Find-Upwards/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/File-Find-Upwards/>.
 
-=head1 AUTHORS
+The development version lives at
+L<http://github.com/hanekomu/File-Find-Upwards/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+=head1 AUTHOR
+
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008-2009 by the author.
+This software is copyright (c) 2008 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
